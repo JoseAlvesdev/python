7de823f1ba822e85076d3c652ab1038f165f1152
josehenriques10 <137122689+josehenriques10@users.noreply.github.com>
2023-08-21 20:37:26 -0300

    exercicio 4 lancado e finalizado

---
+++ Conhecendo a Linguagem de Python/ex004_conversao_tipos.py
@@ -0,0 +1,59 @@
+print(int(8.864983279048778))
+
+print('--------------')
+
+print(float(8))
+
+print('--------------')
+
+preco = 60.50
+
+preco = int(preco)
+
+print(preco)
+
+print('--------------')
+
+preco = 9
+
+preco = float(preco)
+
+print(preco)
+
+print('--------------')
+
+# conversoes para string
+
+"""
+teste de comentario
+sdf
+
+Não gostei
+"""
+
+preco = (str(preco))
+
+print(type(preco))
+
+print('--------------')
+
+preco = (float(preco))
+
+print(preco + 8.0)
+
+print('--------------')
+
+print(type(preco))
+
+preco = int(preco)
+
+print('--------------')
+
+print(100 / 2)
+
+print('--------------')
+print(100 // 2)
+
+
+
+
