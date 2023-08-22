003de0776e535ad9c0ecc85af728b4434823b5ac
josehenriques10 <137122689+josehenriques10@users.noreply.github.com>
2023-08-22 19:24:20 -0300

    lancado e finalizado ex005

---
+++ Conhecendo a Linguagem de Python/ex005_function.py
@@ -0,0 +1,9 @@
+nome = input("Informe o seu nome: ")
+idade = int(input("Informe a sua idade: "))
+
+print(nome, idade)
+print(nome, idade, end="... \n")
+print(nome, idade, sep="#", end="... \n")
+print(nome, idade, sep="#")
+
+print(type(idade))
