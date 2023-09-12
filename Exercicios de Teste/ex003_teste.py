76738a873bf53bb71e9abd3684d6e18ea12edf3c
josehenriques10 <137122689+josehenriques10@users.noreply.github.com>
2023-09-12 11:42:19 -0300

    AT

---
+++ Exercicios de Teste/ex003_teste.py
@@ -0,0 +1,23 @@
+numeros = {1, 2, 3, 4, 5}
+
+frutas = ['maca', 'laranja', 'uva']
+
+usuario = {'nome': 'Choronela', 'idade': 24, 'telefone': '62 99160-5900'}
+
+pessoa = {
+    'josehenriques10@outlook.com': {'nome': 'José', 'idade': 22, 'telefone': '62 99160-5900'}
+}
+
+jogos = ('futsal', 'basque',)
+
+
+matriz = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i'],
+    [1, 2, 3],
+]
+
+print('josehenriques10@outlook.com' in pessoa)
+
+print('nome' in pessoa['josehenriques10@outlook.com'])
