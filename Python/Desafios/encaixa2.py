8439c04389c261ee6179fded68fc4ffafa84a6a5
josehenriques10 <josehenriques10@outlook.com>
2023-10-13 13:20:56 -0300

    Novos exercicios

---
+++ Python/Desafios/encaixa2.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+while(n > 0):
+    A, B = input(" ").split()
+    n = n - 1
+
+    if A[-len(B):]. find (B) != -1:
+        print("encaixa")
+    else:
+        print("nao encaixa")
