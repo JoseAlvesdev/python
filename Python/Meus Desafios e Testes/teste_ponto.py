af1069c10cd8268e9cad1cab3df4a02bf12d6702
josehenriques10 <josehenriques10@outlook.com>
2023-11-18 21:12:17 -0300

    .

--- Python/Meus Desafios e Testes/teste_ponto.py
+++ Python/Meus Desafios e Testes/teste_ponto.py
@@ -1,2 +1,5 @@
-number = int(input('Digite um número: '))
-is_par = number % 2 == 0
+number = int(input('Digite um número: ')); is_par = number % 2 == 0
+if is_par:
+    print('É par!')
+if not is_par:
+    print('É impar!')
