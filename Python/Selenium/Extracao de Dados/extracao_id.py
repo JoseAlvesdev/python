726f7d9f3da88aa0d5609d92b8ac9268165e47fe
josehenriques10 <josehenriques10@outlook.com>
2023-11-26 10:25:50 -0300

    .

---
+++ Python/Selenium/Extracao de Dados/extracao_id.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+browser = webdriver.Chrome()
+service=Service(ChromeDriverManager().install())
+
+browser.get('https://www.infomoney.com.br/')
+
+sleep(2)
+
+dados = browser.find_element(By.ID, 'menu-side').text
+
+print(dados)
