4462245b55dcec4799755d426a2fdfb5fc88ff45
josehenriques10 <josehenriques10@outlook.com>
2023-11-30 11:06:31 -0300

    Novo exercicio

--- sql_alchemy/integration_with_sqlalchemly/app.py
+++ sql_alchemy/integration_with_sqlalchemly/app.py
@@ -1,9 +1,11 @@
-from sqlalchemy import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy import inspect 
 
 base = declarative_base()
 
@@ -19,8 +21,31 @@ class User(base):
         'Address', back_populates='user', cascade='all, delete-orphan'
     )
 
+    def __repr__(self):
+        return f'User(id={self.id}, name={self.name}, fullname={self.fullname})'
+
 
 class Address(base):
-    id = Column(Integer, primary_key=True, auto_increment=True)
+    __tablename__ = 'address'
+
+    id = Column(Integer, primary_key=True)
     email_address = Column(String(30), nullable=False)
     user_id =Column(Integer, ForeignKey('user_acount.id'), nullable=False)
+
+    user = relationship('User', back_populates='address')
+
+    def __repr__(self):
+        return f'Address(id={self.id}, email={self.email_address})'
+    
+
+print(User.__tablename__)
+
+# Conexão com o banco de dados
+engine = create_engine('sqlite://')
+
+# Criando as classe como tabela no banco de dados
+base.metadata.create_all(engine)
+
+#depreciado - será removido em futuro release
+#print(engine.table_names())
+inspetor_engine = inspect(engine)
