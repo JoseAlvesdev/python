c659d45704df072b9b993ee57b38c7bcc2d6d49d
josehenriques10 <josehenriques10@outlook.com>
2024-03-25 21:06:41 -0300

    Nova seçao iniciada.

--- geek_university/python_essencial/secao_13_let_esc_arquivos/leitura.py
+++ geek_university/python_essencial/secao_13_let_esc_arquivos/leitura.py
@@ -13,10 +13,29 @@ https://docs.python.org/3/library/functions.html#open
 
 # OBS: Por padrão, a função open() abre o arquivo para leitura. 
 Este arquivo deve existir, ou então teremos o erro FileNotFoundError.
+
+<_io.TextIOWrapper name='texto.txt' mode='r' encoding='cp1252'>
+
+mode r -> Mode de leitura. r -> read() -> ler (Padrão)
 """
 
 # Exemplo
 
 arquivo = open('texto.txt')
 
-print(arquivo, type(arquivo))
+# print(str(arquivo))
+# print(type(arquivo))
+
+# Para ler o conteúdo de um arquivos após a sua abertura, devemos 
+# utilizar a função read()
+
+ret = arquivo.read()
+
+print(type(ret))
+
+print(ret)
+
+# OBS: O Python, utiliza um recurso para trabalhar com arquivos chamado
+# cursor. Esse cursor, funciona como o cursor quando estamos escrevendo.
+
+# A função read() lê todo o conteúdo do arquivo.
